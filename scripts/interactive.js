21745cb93f6d892e6d6a2f11b6fe902b5697ddc0
Jair Escobar <evitalires@gmail.com>
2019-03-14 17:20:30 -0500

    adding icon,  behavior interactive for icon, and styles.

--- scripts/interactive.js
+++ scripts/interactive.js
@@ -1,10 +1,28 @@
 //Source of code: https://medium.com/@gurjitmehta/smooth-scroll-with-javascript-571283e9a3cd
 
 window.onload = () => {
-    //Main method
-const easeInCubic = function (t) { return t*t*t }
+  ScrollingNav();
+  menu();
+};
+
+function menu() {
+  const menu = document.querySelector(".menu");
+  const burgerButton = document.querySelector('.burger-menu');
+  function toggleClass() {
+    menu.classList.toggle('is-active')
+  }
+  
+  burgerButton.addEventListener('click', () => toggleClass())
+  menu.addEventListener('click', () => toggleClass())
+
+
+}
+
+function ScrollingNav() {
+    //Main method for scroll
+  	const easeInCubic = function (t) { return t*t*t }
  
-const scrollToElem = (startTime, currentTime, duration, scrollEndElemTop, startScrollOffset) => {
+  const scrollToElem = (startTime, currentTime, duration, scrollEndElemTop, startScrollOffset) => {
    const runtime = currentTime - startTime;
    let progress = runtime / duration;
    
@@ -19,15 +37,15 @@ if(runtime < duration){
        scrollToElem(startTime, currentTime, duration, scrollEndElemTop, startScrollOffset);
      })
    }
- }
+  }
 
 
-//select all element with class 'Scroll'
-const scrollElements = document.querySelectorAll('.scroll');
+  //select all element with class 'Scroll'
+  const scrollElements = document.querySelectorAll('.scroll');
 
 
-//Adding event to each element
-scrollElements.forEach( elem => {
+  //Adding event to each element
+  scrollElements.forEach( elem => {
     elem.addEventListener('click', elem => {
         elem.preventDefault();
 
@@ -48,6 +66,7 @@ scrollElements.forEach( elem => {
             scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
         })
     } )
-} )
+  })
 
 }
+
